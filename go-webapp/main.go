710c1337a4c9b4f35a825dabf1182e43234ce6c7
Ankit <ankit.raut@coditas.com>
2023-11-30 10:33:21 +0530

    Added C#,Python,Go,Java and Node projects

---
+++ go-webapp/main.go
@@ -0,0 +1,26 @@
+// main.go
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	// Serve static files
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/blank.html")
+	})
+
+	// Define API endpoints
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/login.html")
+	})
+
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/register.html")
+	})
+
+	// Start the server
+	http.ListenAndServe(":3000", nil)
+	print("Running on: http://localhost:3000")
+}
